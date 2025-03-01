1642bb1d6d17c134390acaeb2d53773e16e364c1
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-03-01 17:26:57 +0100

    custom error message for prod

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
+// import NextError from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({
@@ -15,15 +15,17 @@ export default function GlobalError({
     });
   }, [error]);
 
-  return (
-    <html>
-      <body>
-        {/* `NextError` is the default Next.js error page component. Its type
-        definition requires a `statusCode` prop. However, since the App Router
-        does not expose status codes for errors, we simply pass 0 to render a
-        generic error message. */}
-        <NextError statusCode={0} />
-      </body>
-    </html>
-  );
+  return null;
+
+  // return (
+  //   <html>
+  //     <body>
+  //       {/* `NextError` is the default Next.js error page component. Its type
+  //       definition requires a `statusCode` prop. However, since the App Router
+  //       does not expose status codes for errors, we simply pass 0 to render a
+  //       generic error message. */}
+  //       <NextError statusCode={0} />
+  //     </body>
+  //   </html>
+  // );
 }
