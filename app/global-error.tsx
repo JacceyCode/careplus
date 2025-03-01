a61c04d0997d6e439c847f9290b58842d0ca1e4e
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-03-01 17:14:37 +0100

    custom error message

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -4,9 +4,15 @@ import * as Sentry from "@sentry/nextjs";
 import NextError from "next/error";
 import { useEffect } from "react";
 
-export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
   useEffect(() => {
-    Sentry.captureException(error);
+    Sentry.captureException(error, {
+      extra: { digest: error.digest },
+    });
   }, [error]);
 
   return (
